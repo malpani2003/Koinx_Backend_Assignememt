f5ef13e87daafc8fc95b6de2e69a7f2cd919aa8c
Pranav Malpani <98647622+malpani2003@users.noreply.github.com>
2024-09-01 12:59:42 +0530

    Update app_routes.js

--- Routes/app_routes.js
+++ Routes/app_routes.js
@@ -4,7 +4,9 @@ const {
   saveAndUpdateTransactions,
   getTotalExpenses,
 } = require("../controllers/transactionControllers");
-
+Router.get("/", (req, res) => {
+  res.send("Welcome to the Transactions API");
+});
 Router.get('/transactions/:address', saveAndUpdateTransactions);
 Router.get("/expenses/:address", getTotalExpenses);
 
